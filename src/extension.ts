2e06c8b205d2191b130727188cb5cc9865448083
Hectoralvf <60094746+Hectoralvf@users.noreply.github.com>
2022-12-27 14:03:00 +0100

    Added Unicode symbols if available

--- src/extension.ts
+++ src/extension.ts
@@ -16,14 +16,18 @@ export function activate(context: vscode.ExtensionContext) {
 
   // Create the undo button
   const undoButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-  undoButton.text = 'Undo';
+  // Try to use the Unicode symbol for the undo arrow (U+21A9) as the label for the button
+  const undoLabel = String.fromCodePoint(0x21A9) || 'Undo';
+  undoButton.text = undoLabel;
   undoButton.color = 'var(--vscode-tab-activeBorder)';
   undoButton.command = undoCommand;
   undoButton.show();
 
   // Create the redo button
   const redoButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-  redoButton.text = 'Redo';
+  // Try to use the Unicode symbol for the redo arrow (U+21AA) as the label for the button
+  const redoLabel = String.fromCodePoint(0x21AA) || 'Redo';
+  redoButton.text = redoLabel;
   redoButton.color = 'var(--vscode-tab-activeBorder)';
   redoButton.command = redoCommand;
   redoButton.show();
